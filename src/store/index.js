9410a837aac0315096893a235c9eb9168d1d7978
rumblfn <toshamilgis@gmail.com>
2021-11-16 20:42:10 +0300

    homework7 persistor

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,14 @@ import {chatReducer} from "./chat";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { profileReducer } from "./profile";
 import thunk from 'redux-thunk'
+import storage from 'redux-persist/lib/storage'
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
 const rootReducer = combineReducers({
     chats: chatsReducer,
@@ -11,7 +19,11 @@ const rootReducer = combineReducers({
     profile: profileReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(thunk))
 )
+
+export const persistedStore = persistStore(store)
